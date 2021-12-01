d0d99c8e05e42af02f3205973d5da8d035cccfae
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2021-12-01 01:38:26 -0500

    add remove item functionality

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
     const [items, setItems] = useState([
@@ -10,13 +10,19 @@ const App = () => {
         {id: uuidv4(), text: 'Eggs'},
         {id: uuidv4(), text: 'Yogurt'},
         {id: uuidv4(), text: 'Cucumber'},
-    ])
+    ]);
+
+    const deleteItem = (id) => {
+        setItems(prevItems => {
+            return prevItems.filter(item => item.id != id);
+        });
+    }
 
     return (
         <View style={styles.container}>
-            <Header />
+            <Header/>
             <FlatList data={items}
-                      renderItem={({item}) => <ListItem item={item}/>}
+                      renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>}
             />
         </View>
     );
