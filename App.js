d0c0d96ca4a3a449e3ffaa20fe086e7517eb9579
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2021-12-01 01:23:11 -0500

    render shopping list items

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
-import Header from './components/Header'
+import React, {useState} from "react";
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+    const [items, setItems] = useState([
+        {id: uuidv4(), text: 'Potatoes'},
+        {id: uuidv4(), text: 'Eggs'},
+        {id: uuidv4(), text: 'Yogurt'},
+        {id: uuidv4(), text: 'Cucumber'},
+    ])
+
     return (
         <View style={styles.container}>
             <Header />
+            <FlatList data={items}
+                      renderItem={({item}) => <Text>{item.text}</Text>}
+            />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
