6dccb14c0f8dc0e23c99f457850957292356199d
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2021-12-01 01:29:04 -0500

    add list item component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
         <View style={styles.container}>
             <Header />
             <FlatList data={items}
-                      renderItem={({item}) => <Text>{item.text}</Text>}
+                      renderItem={({item}) => <ListItem item={item}/>}
             />
         </View>
     );
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {View, Text, StyleSheet} from 'react-native';
 
-
 const Header = ({title}) => {
     return (
         <View style={styles.header}>
